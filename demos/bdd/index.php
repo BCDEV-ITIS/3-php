02b83b1eba303392896ab1be160cb7d55a6636f6
glodie <glodie.tshimini@gmail.com>
2026-01-22 14:15:50 +0100

    feat: start PDO

---
+++ demos/bdd/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Se connecter à la base de données à l'aide de la classe PDO
+ * Inserer un ou plusieurs utilisateur à l'aide de PDO et de la requête INSERT
+ * @see https://blog.crea-troyes.fr/3368/pdo-en-php-un-tutoriel-complet-avec-des-exemples-concrets/
+ */
+
+
